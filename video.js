e9670cb16ea8063b0b30187bb62891c4cd6af1f0
khiem.v.le <lek@deea.thebuffalogroup.com>
2019-07-12 15:50:07 -0400

    first commit

---
+++ video.js
@@ -0,0 +1,16 @@
+
+  		$(document).ready(function () {
+		    $(".arrow-right").bind("click", function (event) {
+		        event.preventDefault();
+		        $(".vid-list-container").stop().animate({
+		            scrollLeft: "+=336"
+		        }, 750);
+		    });
+		    $(".arrow-left").bind("click", function (event) {
+		        event.preventDefault();
+		        $(".vid-list-container").stop().animate({
+		            scrollLeft: "-=336"
+		        }, 750);
+		    });
+		});
+  	
